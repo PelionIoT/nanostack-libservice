a11527db325cac7a142d9a2727a52fac336792e1
Antti Kauppila <antti.kauppila@arm.com>
2016-11-10 14:52:52 +0200

    nsdynmemlib code harmonized
    unittests updated

--- test/libService/unittest/nsdynmem/dynmemtest.cpp
+++ test/libService/unittest/nsdynmem/dynmemtest.cpp
@@ -428,6 +428,48 @@ TEST(dynmem, test_invalid_pointer_freed) {
     free(heap);
 }
 
+TEST(dynmem, test_merge_corrupted_previous_block) {
+    uint16_t size = 1000;
+    uint8_t *heap = (uint8_t*)malloc(size);
+    uint8_t *p;
+    CHECK(NULL != heap);
+    reset_heap_error();
+    ns_dyn_mem_init(heap, size, &heap_fail_callback, NULL);
+    CHECK(!heap_have_failed());
+
+    int *ptr = (int *)ns_dyn_mem_alloc(4);
+    int *ptr2 = (int *)ns_dyn_mem_alloc(4);
+    ns_dyn_mem_free(ptr);
+    ptr = ptr2 - 2;
+    *ptr = -2;
+    ns_dyn_mem_free(ptr2);
+
+    CHECK(NS_DYN_MEM_HEAP_SECTOR_CORRUPTED == current_heap_error);
+
+    free(heap);
+}
+
+TEST(dynmem, test_free_corrupted_next_block) {
+    uint16_t size = 1000;
+    uint8_t *heap = (uint8_t*)malloc(size);
+    uint8_t *p;
+    CHECK(NULL != heap);
+    reset_heap_error();
+    ns_dyn_mem_init(heap, size, &heap_fail_callback, NULL);
+    CHECK(!heap_have_failed());
+
+    int *ptr = (int *)ns_dyn_mem_temporary_alloc(4);
+    int *ptr2 = (int *)ns_dyn_mem_temporary_alloc(4);
+    ns_dyn_mem_free(ptr);
+    ptr = ptr2 + 2;
+    *ptr = -2;
+    ns_dyn_mem_free(ptr2);
+
+    CHECK(NS_DYN_MEM_HEAP_SECTOR_CORRUPTED == current_heap_error);
+
+    free(heap);
+}
+
 //NOTE! This test must be last!
 TEST(dynmem, uninitialized_test){
     ns_dyn_mem_alloc(4);
