bed42d5bec32a247bea2c7b7876773be7d190fe0
Antti Kauppila <antti.kauppila@arm.com>
2016-12-21 11:47:28 +0200

    Added with Kevin suprpess MACRO to disable wrong warnings in split builds
    - In some split build, some functions have been defined as dummy
      and that causes some stupid compilation warnings
    - This has no effect in release builds as all functions are defined.

--- source/nsdynmemLIB/nsdynmemLIB.c
+++ source/nsdynmemLIB/nsdynmemLIB.c
@@ -171,7 +171,6 @@ static void *ns_dyn_mem_internal_alloc(const int16_t alloc_size, int direction)
 {
 #ifndef STANDARD_MALLOC
     int *block_ptr = NULL;
-    size_t block_data_size = 0;
 
     platform_enter_critical();
 
@@ -204,7 +203,7 @@ static void *ns_dyn_mem_internal_alloc(const int16_t alloc_size, int direction)
         goto done;
     }
 
-    block_data_size = -*block_ptr;
+    size_t block_data_size = -*block_ptr;
     if (block_data_size >= (data_size + 2 + HOLE_T_SIZE)) {
         int hole_size = block_data_size - data_size - 2;
         int *hole_ptr;
